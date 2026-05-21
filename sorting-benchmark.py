cefa8fb0b8d9ad11076b0652289039b48231822f
Siddharth Saxena <Siddharthsaxena555@gmail.com>
2026-05-21 20:09:18 +0530

    Create sorting-benchmark.py

---
+++ sorting-benchmark.py
@@ -0,0 +1,24 @@
+import time 
+import random
+
+# 1. Bubble sort
+def bubble_sort(arr)
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j=1]:
+                arr[j], arr[j=1] = arr[j+1], arr[j]
+
+# 2. Insertion sort
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i-1
+        while j >= 0 and key < arr [j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+# 3. Merge Sort
+def merge_sort(arr):
+  
